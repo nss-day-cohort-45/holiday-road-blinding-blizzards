d14c7967f768e70762ee2fbf78a1ab7ade9f0b4b
pazia008 <pazia008@gmail.com>
2020-12-15 13:41:27 -0600

    worked on the national park modules

---
+++ scripts/parks/nationalParkHTMLConverter.js
@@ -0,0 +1,22 @@
+export const nationalParkHTMLConverter = (park) => {
+    return `
+    <article class="preview__nationalPark">
+        <h2 class="preview__parkName">Park Name: ${park.fullName}</h2>
+        <button id="parkIdentifier--${park.id}">Details</button>
+    </article>
+    `
+}
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    const [splitID, indexOne] = clickEvent.target.id.split("--")
+    if ("parkIdentifier" === splitID) {
+        const customEvent = new CustomEvent("parkDetailsButtonClicked", {
+            detail: {
+                parkDetailsChosen: indexOne
+            }
+        })
+        eventHub.dispatchEvent(customEvent)
+    }
+})
